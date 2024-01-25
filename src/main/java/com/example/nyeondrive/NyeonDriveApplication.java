37c104e70f4875721b5dcdb68bd25982bbbf2681
김강년 <64246800+ekek54@users.noreply.github.com>
2024-01-25 19:37:07 +0900

    chore: 설정 파일(.properties) ignore 등록

---
+++ src/main/java/com/example/nyeondrive/NyeonDriveApplication.java
@@ -0,0 +1,13 @@
+package com.example.nyeondrive;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NyeonDriveApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NyeonDriveApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/nyeondrive/NyeonDriveApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.nyeondrive;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NyeonDriveApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
