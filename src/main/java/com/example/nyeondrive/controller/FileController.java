118304559ce9ba666c06526d8ae21848b4b00b0c
김강년 <64246800+ekek54@users.noreply.github.com>
2024-02-13 12:11:35 +0900

    feat: 스트림 업로드, 파일 메타데이터 저장 라우팅 로직 작성

--- src/main/java/com/example/nyeondrive/controller/FileController.java
+++ src/main/java/com/example/nyeondrive/controller/FileController.java
@@ -1,24 +1,38 @@
 package com.example.nyeondrive.controller;
 
+import com.example.nyeondrive.dto.request.FileRequestDto;
 import com.example.nyeondrive.entity.File;
-import com.example.nyeondrive.repository.FileRepository;
-import jakarta.servlet.ServletException;
+import com.example.nyeondrive.service.FileService;
+import com.example.nyeondrive.service.StorageService;
 import jakarta.servlet.http.HttpServletRequest;
-import jakarta.servlet.http.Part;
 import java.io.IOException;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/api/files")
 public class FileController {
-    private final FileRepository fileRepository;
+    private final FileService fileService;
+    private final StorageService storageService;
 
-    public FileController(FileRepository fileRepository) {
-        this.fileRepository = fileRepository;
+    public FileController(FileService fileService, StorageService storageService) {
+        this.fileService = fileService;
+        this.storageService = storageService;
     }
 
+
+    @PostMapping
+    public String saveFile(@RequestBody FileRequestDto fileRequestDto) {
+        File file = File.builder()
+                .fileName(fileRequestDto.getName())
+                .parent(fileService.findFile(fileRequestDto.getParentId()).orElseThrow(RuntimeException::new))
+                .build();
+        fileService.saveFile(file);
+        return "saveFile";
+    }
 
     /**
      * 단일 파일 업로드에 사용
@@ -26,14 +40,23 @@ public class FileController {
      * @return File
      */
     @PostMapping(value = "/upload", params = "mode=stream")
-    public String streamUpload(HttpServletRequest request) throws IOException, ServletException {
-        String contentType = request.getContentType();
-        Long contentLength = (long) request.getContentLength();
-        String fileName = request.getHeader("File-Name");
-        String relativePath = request.getHeader("Relative-Path");
-        File file = new File(fileName, contentType, contentLength, request.getInputStream());
-        fileRepository.uploadFile(file);
+    public String streamUpload(
+            @RequestHeader("File-Name") String fileName,
+            @RequestHeader("Content-Type") String contentType,
+            @RequestHeader("Content-Length") Long contentLength,
+            @RequestHeader("Parent-Id") Long parentId,
+            HttpServletRequest request
+    ) throws IOException {
+        File parent = fileService.findFile(parentId).orElseThrow(RuntimeException::new);
+        File file = File.builder()
+                .fileName(fileName)
+                .contentType(contentType)
+                .size(contentLength)
+                .parent(parent)
+                .inputStream(request.getInputStream())
+                .build();
+        fileService.saveFile(file);
+        storageService.uploadFile(file);
         return "streamUpload";
     }
-
 }
