5a368653de1939c42919e44741282564c515c1cb
김강년 <64246800+ekek54@users.noreply.github.com>
2024-03-01 01:25:47 +0900

    style: 불필요 생성자 제거

--- src/main/java/com/example/nyeondrive/dto/request/StreamUploadRequestDto.java
+++ src/main/java/com/example/nyeondrive/dto/request/StreamUploadRequestDto.java
@@ -1,18 +1,11 @@
 package com.example.nyeondrive.dto.request;
 
 import lombok.Builder;
-
+@Builder
 public record StreamUploadRequestDto(
         String fileName,
         String contentType,
         Long contentLength,
         Long parentId
 ) {
-    @Builder
-    public StreamUploadRequestDto(String fileName, String contentType, Long contentLength, Long parentId) {
-        this.fileName = fileName;
-        this.contentType = contentType;
-        this.contentLength = contentLength;
-        this.parentId = parentId;
-    }
 }
