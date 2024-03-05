885115f0009e738edbd27c2993e46430e53d78ad
김강년 <64246800+ekek54@users.noreply.github.com>
2024-03-05 18:32:50 +0900

    chore: 패키지 구조 변경 도메인 패키지 추가

--- src/main/java/com/example/nyeondrive/file/dto/request/CreateFileRequestDto.java
+++ src/main/java/com/example/nyeondrive/file/dto/request/CreateFileRequestDto.java
@@ -3,21 +3,21 @@ package com.example.nyeondrive.file.dto.request;
 import com.example.nyeondrive.file.dto.service.CreateFileDto;
 import jakarta.validation.constraints.NotNull;
 
-public record CreateFileRequestDto(
-        @NotNull
-        String name,
-        @NotNull
-        Long parentId,
-        @NotNull
-        String contentType,
-        Boolean isTrashed
+public record CreateFileRequestDto (
+    @NotNull
+    String name,
+    @NotNull
+    Long parentId,
+    @NotNull
+    String contentType,
+    Boolean isTrashed
 ) {
     public CreateFileDto toCreateFileDto() {
         return new CreateFileDto(
-                name,
-                parentId,
-                contentType,
-                isTrashedIfNullIsFalse()
+            name,
+            parentId,
+            contentType,
+            isTrashedIfNullIsFalse()
         );
     }
 
