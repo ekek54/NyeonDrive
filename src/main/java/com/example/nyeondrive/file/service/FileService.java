4224f05dee2eb526864b22c90ceddf3fd0fc0a03
김강년 <64246800+ekek54@users.noreply.github.com>
2024-08-02 19:56:54 +0900

    chore: 주석 추가

--- src/main/java/com/example/nyeondrive/file/service/FileService.java
+++ src/main/java/com/example/nyeondrive/file/service/FileService.java
@@ -72,14 +72,14 @@ public class FileService {
     }
 
     public FileDto findFile(Long fileId) {
+        // 상위 파일 삭제 여부 확인
         File fileReference = fileRepository.getReferenceById(fileId);
         if (isAncestorTrashed(fileReference)) {
             throw new BadRequestException("Parent file is trashed");
         }
+        // 파일 및 부모 아이디 찾기
         File file = fileRepository.findById(fileId).
                 orElseThrow(() -> new NotFoundException("File not found"));
-        Optional<FileClosure> parentClosure = fileClosureRepository
-                .findWithAncestorByDescendantAndDepth(file, 1L);
         Long parentId = findParent(file) == null ? null : findParent(file).getId();
         return FileDto.of(parentId, file);
 
@@ -91,10 +91,20 @@ public class FileService {
         return !trashedAncestorClosures.isEmpty();
     }
 
+    /**
+     * 파일의 부모 파일을 찾는다.
+     * 캐싱된 부모 파일이 없으면 DB에서 조회하며 캐싱한다.
+     * 부모 파일은 아이디만 사용되는 경우가 많아 프록시 객체로 반환한다.
+     * @param file 파일
+     * @return 부모 파일
+     */
     private File findParent(File file) {
+        // 캐싱된 부모 리턴
         if (file.getParent() != null) {
             return file.getParent();
         }
+
+        // 부모 파일 조회
         Optional<FileClosure> parentClosure = fileClosureRepository.findWithAncestorByDescendantAndDepth(file, 1L);
         File parentReference = parentClosure
                 .map(FileClosure::getAncestor)
@@ -103,6 +113,13 @@ public class FileService {
         return file.getParent();
     }
 
+    /**
+     * 드라이브 생성
+     * 이미 드라이브가 존재하면 예외 발생
+     * 드라이브의 부모는 Null이다.
+     * @param userId 사용자 아이디
+     * @return 생성된 드라이브
+     */
     public FileDto createDrive(UUID userId) {
         fileRepository.findByOwnerIdAndContentType(userId, FileType.DRIVE_CONTENT_TYPE)
                 .ifPresent(file -> {
@@ -139,8 +156,14 @@ public class FileService {
         return FileDto.of(parentId, file);
     }
 
+    /**
+     * 파일 이동
+     * 파일의 부모를 변경한다.
+     * 파일의 부모가 변경되면 클로저를 갱신한다.
+     * @param file 파일
+     * @param newParentId 새 부모 파일 아이디
+     */
     private void moveFile(File file, Long newParentId) {
-        System.out.println("moveFile: " + file.getId() + " -> " + newParentId);
         File newParent = fileRepository.findWithAncestorClosuresById(newParentId)
                 .orElseThrow(() -> new BadRequestException("Parent not found"));
         validateParent(file, newParent);
@@ -152,18 +175,31 @@ public class FileService {
         if (parent.isFile()) {
             throw new BadRequestException("Parent is not a folder");
         }
-
         if (detectCycle(file, parent)) {
             throw new BadRequestException("cycle detected");
         }
     }
 
+
+    /**
+     * 새로운 폴더로 옮겨 졌을 때 순환 구조가 발생하는지 확인
+     * 새로운 폴더의 조상에 현재 파일이 이미 존재하는지 확인
+     * @param file
+     * @param parent
+     * @return
+     */
     private boolean detectCycle(File file, File parent) {
         return parent.getAncestorClosures()
                 .stream()
                 .anyMatch(ancestorClosure -> ancestorClosure.getAncestor().equals(file));
     }
 
+
+    /**
+     * 파일의 서브트리를 전체 트리에서 분리한다.
+     * <서브트리의 조상 - 서브트리에 포함된 모든 노드> 관계의 클로저 삭제
+     * @param file
+     */
     private void detachSubtree(File file) {
         loadAllClosures(file);
         List<File> nodesInSubtree = file.getDescendantClosures()
@@ -178,6 +214,12 @@ public class FileService {
         fileClosureRepository.deleteAllInBatchByAncestorInAndDescendantIn(ancestorsOfSubtreeRoot, nodesInSubtree);
     }
 
+    /**
+     * 파일의 서브트리를 새로운 부모에 연결한다.
+     * <새로운 부모의 조상 - 서브트리에 포함된 모든 노드> 관계의 클로저 생성
+     * @param file
+     * @param parent
+     */
     private void attachSubtree(File file, File parent) {
         log.trace("attachSubtree: {}", file.getId());
         List<FileClosure> newClosures = new ArrayList<>();
